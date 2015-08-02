63fe65364da61a9be490342fd70ee8192c01647e
Gregory Thompson <gregt87@gmail.com>
2015-08-02 18:53:33 -0400

    added library which adds public key third party caveats

--- test/index.js
+++ test/index.js
@@ -1,9 +1,47 @@
 'use strict';
-var assert = require('assert');
-var publicKeyMacaroons = require('../lib');
 
-describe('public-key-macaroons', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+var expect = require('chai').expect, 
+  macaroons = require('macaroons.js'),
+  _ = require("lodash"),
+  fs = require('fs'),
+  ursa = require('ursa'),
+  MacaroonsBuilder = macaroons.MacaroonsBuilder,
+  deserializeFn = MacaroonsBuilder.deserialize,
+  publicKeyMacaroons = require('../lib');
+
+describe("public-key-macaroons", function () {
+  describe("third party caveats", function() {
+
+    var fooPublicKeyPem = fs.readFileSync(__dirname + "/pem/foo.pub.pem", "utf8");
+    var fooPrivateKeyPem = fs.readFileSync(__dirname + "/pem/foo.priv.pem", "utf8");
+    var serializedMac = new MacaroonsBuilder("thing1.com", "my secret", "identifier")
+      .getMacaroon()
+      .serialize();
+
+    it("bind encrypted third party caveat", function () {
+      var caveatKey = "my caveat secret";
+      var message = "account = 11238";
+      var actualMessage = "caveat_key = " + caveatKey + "\n" + "message = " + message + "\n";
+
+      var macAndDischarge = publicKeyMacaroons.addPublicKey3rdPartyCaveat(serializedMac, "thing2.com", caveatKey, message, fooPublicKeyPem);
+
+      var inspectedMac = deserializeFn(macAndDischarge.macaroon).inspect();
+      var lineWithEncoding = _.find(inspectedMac.split("\n"), function (line) {return line.indexOf("cid enc = ") !== -1; });
+      var encoding = lineWithEncoding.split("cid enc = ")[1];
+
+      expect(ursa.createPrivateKey(fooPrivateKeyPem).decrypt(encoding, 'base64', 'utf8')).to.equal(actualMessage);
+    });
+
+    it("discharge valid", function () {
+      var caveatKey = "my caveat secret";
+      var message = "account = 11238";
+      var actualMessage = "caveat_key = " + caveatKey + "\n" + "message = " + message + "\n";
+      var macAndDischarge = publicKeyMacaroons.addPublicKey3rdPartyCaveat(serializedMac, "thing2.com", caveatKey, message, fooPublicKeyPem);
+
+      var encryptedDischarge = macAndDischarge.discharge;
+      console.log("encryptedDischarge = %j", encryptedDischarge);
+
+      expect(ursa.createPrivateKey(fooPrivateKeyPem).decrypt(encryptedDischarge, 'base64', 'utf8')).to.equal(actualMessage);
+    });
   });
 });
